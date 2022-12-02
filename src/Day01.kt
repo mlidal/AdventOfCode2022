a8bcdfbe826819d102d57437a7b44d7653ecc5fc
Mathias Mølster Lidal <mathias.lidal@bouvet.no>
2022-12-02 09:34:32 +0100

    Solution for Day 1 of AdventOfCode

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,39 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val calories = mutableListOf<Int>()
+        var calorieSum = 0
+        input.forEach {
+            if (it.isNotBlank()) {
+                calorieSum += it.toInt()
+            } else {
+                calories.add(calorieSum)
+                calorieSum = 0
+            }
+        }
+        calories.add(calorieSum)
+        return calories.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val calories = mutableListOf<Int>()
+        var calorieSum = 0
+        input.forEach {
+            if (it.isNotBlank()) {
+                calorieSum += it.toInt()
+            } else {
+                calories.add(calorieSum)
+                calorieSum = 0
+            }
+        }
+        calories.add(calorieSum)
+        calories.sortDescending()
+        return calories.take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
